e753ad0e5987bb920dba9c0e671f7950317e2626
Tomoyuki Sakurai <y@trombik.org>
2025-12-24 11:10:03 +0700

    doc: document eil_check_idf_has_esp_drivers and eil_check_idf_features

--- cmake/eil_check_idf_features.cmake
+++ cmake/eil_check_idf_features.cmake
@@ -3,11 +3,13 @@ if(COMMAND eil_check_idf_features)
     return()
 endif()
 
-
-#[=[@doc
-
-#]=]
-
+# @brief Executes all available IDF feature validation checks
+#
+# This macro invokes all feature validation checks defined in
+# `eil_check_idf_features.cmake` and exports any variables they define.
+#
+# @note Intended primarily for testing purposes. In production code,
+#       explicitly call only the specific checks your project requires.
 macro(eil_check_idf_features)
     message(DEBUG "eil_check_idf_features")
     message(DEBUG "Running IDF feature checks for version ${IDF_VERSION}")
@@ -27,43 +29,38 @@ macro(eil_check_idf_features)
     endforeach()
 endmacro()
 
-#[=[@doc
-
-Checks if the esp-idf version supports `esp_driver_*` components. Exports
-a boolean variable,`IDF_HAS_ESP_DRIVERS`.
-
-Before version 5.3, the conponent for peripherials was a monolithic `driver`.
-Since version 5.3, each driver, such as `gpio` has its own component, i.e.,
-`esp_driver_gpio`.
-
-When a source includes `driver/gpio.h":
-
-```cmake
-eil_check_idf_has_esp_drivers()
-
-if(${IDF_HAS_ESP_DRIVERS)
-    set(REQUIRED_COMPONENTS esp_driver_gpio freertos log esp_timer)
-else()
-    set(REQUIRED_COMPONENTS driver freertos log esp_timer)
-endif()
-```
-
-When `IDF_HAS_ESP_DRIVERS` is true, add `esp_driver_*` to REQUIRES or
-PRIV_REQUIRES when calling idf_component_register().
-
-When false, add `driver` to REQUIRES or PRIV_REQUIRES.
-
-See Migration from 5.2 to 5.3 for more details.
-https://docs.espressif.com/projects/esp-idf/en/release-v6.0/esp32/migration-guides/release-5.x/5.3/peripherals.html
-
-REQUIRES should be set to all components whose header files are
-`#included` from the public header files of this component.
-
-PRIV_REQUIRES should be set to all components whose header files are
-`#included` from any source files in this component, unless already listed
-in REQUIRES.
-
-#]=]
+# @brief Checks if the esp-idf version supports `esp_driver_*` components. Exports a boolean variable,`IDF_HAS_ESP_DRIVERS`.
+#
+# Before version 5.3, the conponent for peripherials was a monolithic `driver`.
+# Since version 5.3, each driver, such as `gpio` has its own component, i.e.,
+# `esp_driver_gpio`.
+#
+# See
+# [Migration from 5.2 to 5.3](https://docs.espressif.com/projects/esp-idf/en/release-v6.0/esp32/migration-guides/release-5.x/5.3/peripherals.html)
+# for more details.
+#
+# When a source includes `driver/gpio.h":
+#
+# ```cmake
+# eil_check_idf_has_esp_drivers()
+#
+# if(${IDF_HAS_ESP_DRIVERS)
+#     set(REQUIRED_COMPONENTS esp_driver_gpio freertos log esp_timer)
+# else()
+#     set(REQUIRED_COMPONENTS driver freertos log esp_timer)
+# endif()
+# ```
+#
+# @note When `IDF_HAS_ESP_DRIVERS` is true, add `esp_driver_*` to `REQUIRES` or
+#       `PRIV_REQUIRES` when calling `idf_component_register()`.
+#       When false, add `driver` to REQUIRES or PRIV_REQUIRES.
+#
+# @note `REQUIRES` should be set to all components whose header files are
+#       `#included` from the public header files of this component.
+#
+# @note `PRIV_REQUIRES` should be set to all components whose header files are
+#       `#included` from any source files in this component, unless already listed
+# in REQUIRES.
 macro(eil_check_idf_has_esp_drivers)
     # Check for esp_driver_* support (ESP-IDF ≥ 5.3)
     set(IDF_HAS_ESP_DRIVERS FALSE)
